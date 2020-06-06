31d405eddb5917835c59462a105a7aaf3734fe3f
Nur Cahyo <nurcahyoputro0@gmail.com>
2020-06-06 07:06:12 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'angular-dev-trial';
+  contents = [];
+
+  ngOnInit() {
+    this.cloneContent(12);
+  }
+
+  cloneContent(count: number) {
+    for (let i = 0; i < count; i++) {
+      this.contents.push({});
+    }
+  }
+
 }
